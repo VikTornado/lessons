1e2cc4248c9e16e5f6f430017ce798e93f0be39a
Glo_study <3809164@gmail.com>
2022-03-26 19:46:11 +0200

    lesson2

--- Glo1/js/scripr.js
+++ Glo1/js/scripr.js
@@ -1,11 +1,18 @@
 let title = "GloAcademy";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-
-alert('hello world!!!')
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 77;
+let rollback = Math.floor(Math.random() * 100);
+let fullPrice = 99;
+let adaptive = true;
 
 
-console.log('Lesson01');
+console.log("Название проекта " + title + " - тип данных " + (typeof (title)));
+console.log("Хочу заработать " + fullPrice + " долларов " + " - тип данных " + (typeof (fullPrice)));
+console.log("Булевое значение " + adaptive + " - тип данных " + (typeof (adaptive)));
+console.log("Длина строки screens - " + screens.length + " символ");
+console.log("Стоимость верстки экранов " + (screenPrice) + " рублей / долларов / гривен / юани");
+console.log("Стоимость разработки сайта " + (fullPrice) + " рублей / долларов / гривен / юани");
+console.log(screens.toLocaleLowerCase());
+console.log(screens.split(" "));
+console.log("Процент отката посреднику за работу = " + Math.floor(fullPrice * (rollback / 1000)));
+
